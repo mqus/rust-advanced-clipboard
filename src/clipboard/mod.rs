e757b5e3476b38aa63abfc2ae3dbf6d9342366c1
Markus Richter <markus.richter@hu-berlin.de>
2017-12-15 01:25:27 +0100

    someshit - trait vs struct

--- src/clipboard/mod.rs
+++ src/clipboard/mod.rs
@@ -17,7 +17,7 @@ pub trait Transferable: fmt::Debug{
     fn is_flavour_supported(&self,flavour: &Mime) ->bool{
         self.get_data_flavours().contains(flavour)
     }
-    fn get_data(&self,flavour:&Mime)->&[u8];
+    fn get_data(&self,flavour:&Mime)->Option<&[u8]>;
 }
 
 pub trait ClipboardObserver: fmt::Debug{
@@ -26,7 +26,7 @@ pub trait ClipboardObserver: fmt::Debug{
 
 pub struct Clipboard {
     name: String,
-    owner: Option<Weak<RefCell<ClipboardOwner>>>,
+    owner: Option<Weak<RefCell<Box<ClipboardOwner>>>>,
     content: Option<Box<Transferable>>,
     listeners: Vec<Weak<RefCell<ClipboardObserver>>>
 }
@@ -45,7 +45,7 @@ impl Clipboard{
         return self.name.as_ref()
     }
 
-    pub fn set_contents(&mut self, content:Box<Transferable>, owner:&Rc<RefCell<ClipboardOwner>>) {
+    pub fn set_contents(&mut self, content:Box<Transferable>, owner:&Rc<RefCell<Box<ClipboardOwner>>>) {
         if let Some(ref old_owner)=self.owner{
             if let Some(old_owner) = old_owner.upgrade(){
                 if Rc::ptr_eq(&old_owner,owner) {
@@ -87,7 +87,7 @@ impl Clipboard{
         }
     }
 
-    pub fn get_data(&self,flavour:&Mime)->Result<&[u8],error::NoDataError>{
+    pub fn get_data(&self,flavour:&Mime)->Result<Option<&[u8]>,error::NoDataError>{
         match &self.content{
             &None => Err(error::NoDataError{}),
             &Some(ref x) => Ok(x.as_ref().get_data(flavour))
@@ -107,22 +107,105 @@ impl Clipboard{
         });
     }
 }
+mod tests{
+    use mime::Mime;
+    use super::*;
 
-#[test]
-fn create(){
-    let c=Clipboard::new("1".to_string());
 
-    assert!(match c.get_contents() {
-        None => {true},
-        Some(_) => {false},
-    });
-    assert!(match c.get_data(&Mime::from_str("text/plain").expect("noMime")) {
-        Ok(_) => false,
-        Err(_) => true,
-    });
 
-    assert_eq!(c.available_data_flavours().len(),0)
+    macro_rules! bootstrap {
+        () => (
+
+        //define a simple Data type
+        #[derive(Debug)]
+        struct Data<'a>{
+            data:&'a mut [u8],
+            types:&'a mut[Mime]
+        }
+        impl <'a> Transferable for Data<'a>{
+            fn get_data_flavours(&self) -> &[Mime] {
+                &self.types
+            }
+
+            fn get_data(&self, flavour: &Mime) -> Option<&[u8]> {
+                if self.is_flavour_supported(flavour){
+                    Some(&self.data)
+                }else {
+                    None
+                }
+            }
+        }
+        //create a simple Owner type:
+
+        struct Owner{
+            owns:bool
+        }
+
+        impl Owner{
+            pub fn owns_now(&mut self,val:bool){
+                self.owns=val;
+            }
+        }
+
+        impl ClipboardOwner for Owner{
+            fn lost_ownership(&mut self, source: &Clipboard, new_content: Option<&Transferable>) {
+                self.owns=false;
+            }
+        }
+        );
+    }
+
+    #[test]
+    fn create(){
+        let c=Clipboard::new("1".to_string());
+
+        assert!(match c.get_contents() {
+            None => {true},
+            Some(_) => {false},
+        });
+        assert!(match c.get_data(&Mime::from_str("text/plain").expect("noMime")) {
+            Ok(_) => false,
+            Err(_) => true,
+        });
+
+        assert_eq!(c.available_data_flavours().len(),0)
+
+    }
+    #[test]
+    fn testWithEmptyData(){
+
+        bootstrap!();
+
+        //create a Clipboard
+
+        let c = &mut Clipboard::new("TestClipboard2".to_string());
+        let o1=Owner{
+            owns: false,
+        };
+
+        let o2=Owner{
+            owns: false,
+        };
+
+        let d1=Data{
+            data: &mut [],
+            types: &mut [],
+        };
+        let boxed_o1=Rc::new(RefCell::new(Box::new(o1)));
+        c.set_contents(Box::new(d1),&boxed_o1 as &Rc<RefCell<Box<ClipboardOwner>>>);
+        let o1x=boxed_o1.borrow();
+        assert!(true);
+        assert!(match c.get_contents() {
+            None => false,
+            Some(_) => true,
+        });
+        assert!(match c.get_data(&Mime::from_str("text/plain").expect("noMime")) {
+            Ok(_) => true,
+            Err(_) => false,
+        });
+
+
+    }
 
 }
-
 
